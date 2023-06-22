daa4fbf739c5442e83cf5293a63d7b6e0928133f
Isa <iruizord@gmail.com>
2023-06-22 08:53:29 -0600

    setting up html templates for bets and buds

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Category} = require('../models');
+const {User, Category} = require('../models');
 
 router.get('/', async(req, res) => {
     try{
@@ -18,5 +18,21 @@ router.get('/', async(req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/buds', async function(req, res) {
+try {
+    const categories = await Category.findAll()
+    console.log(categories);
+    const parseData = categories.map((category) => category.get({plain:true}));
+    console.log(parseData);
+    res.render('buds', {parseData})
+} catch (error) {
+    res.json(error)
+}
+})
+
+router.get('/bets', function(req, res) {
+    res.render('bets', {nums:[{value:9},{value:8},{value:3},{value:7}]})
+}) 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+/* import */ 
 const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
-const PORT = process.env.PORT || 3001;
 const routes = require('./controllers');
+const expressHandlebars = require('express-handlebars');
+
 
 const app = express();
+const hbs = expressHandlebars.create();
+const PORT = process.env.PORT || 3001;
+
+
+/*middleware*/
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(routes);
 
+
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-});
+app.listen(PORT, () => console.log('Now listening'));});
